35ebdb3e83e62eb8f1ea8b5cbfd92be7f9d29e73
Yotam Leshem <leshemyotam@gmail.com>
2020-09-08 18:21:45 +0300

    initial commit

---
+++ src/main/java/com/sb/messageHashStore/MessageHashStoreApp.java
@@ -0,0 +1,12 @@
+package com.sb.messageHashStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageHashStoreApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageHashStoreApp.class, args);
+    }
+}
